19ff1f363753830399f96d4462ee645aeec6b350
Ableytner <ableit190030@sr.htlweiz.at>
2023-03-02 17:29:45 +0100

    debugging

--- src/iridium_server.py
+++ src/iridium_server.py
@@ -1,10 +1,11 @@
 import asyncio
 import logging
 import socket
+import time
 import json
 import os
 
-from protocol import MinecraftProtocol, HandshakePacket, StatusRequestPacket, StatusResponsePacket
+from protocol import MinecraftProtocol, HandshakePacket, StatusRequestPacket, StatusResponsePacket, PingRequestPacket, PingResponsePacket
 
 BUFFER_SIZE = 65536
 
@@ -29,16 +30,47 @@ class IridiumServer():
         # print(self._parse_handshake(await client_reader.read()))
 
         mcprot = MinecraftProtocol(client_reader, client_writer)
-        conn_info = await mcprot.read_packet(StatusRequestPacket)
-        await conn_info.load()
-        
-        status = {
-            "version": {
-                "name": "1.12.2",
-                "protocol": 759
-            },
-        }
-        await mcprot.write_packet(StatusResponsePacket(status))
+        conn_info = await mcprot.read_packet(HandshakePacket)
+        await conn_info.load() # times out
+
+        if conn_info.is_status_next():
+            print("Next is status, sending...")
+            await mcprot.read_packet(StatusRequestPacket)
+
+            retr_time = time.time()
+            status = {
+                "online": False,
+                "host": "ableytner.ddns.net",
+                "port": self.port,
+                "eula_blocked": False,
+                "retrieved_at": int(retr_time),
+                "expires_at": int(retr_time) + 6000,
+                "version": {
+                    "name_raw": "1.7.10",
+                    "name_clean": "1.7.10",
+                    "name_html": "<span><span style=\"color: #ffffff;\">1.7.10</span></span>",
+                    "protocol": 5
+                },
+                "players": {
+                    "online": 0,
+                    "max": 20,
+                    "list": []
+                },
+                "motd": {
+                    "raw": "IridiumMC server!",
+                    "clean": "IridiumMC server!",
+                    "html": "<span><span style=\"color: #ffffff;\">IridiumMC server! </span>"
+                },
+                "icon": 'null',
+                "mods": []
+            }
+            await mcprot.write_packet(StatusResponsePacket(status))
+            print("wrote status packet")
+            ping = await mcprot.read_packet(PingRequestPacket)
+            await mcprot.write_packet(PingResponsePacket(time=ping.time))
+            mcprot.close()
+        else:
+            print("Login...")
 
     def _parse_handshake(self, request) -> dict:
         data = {}
