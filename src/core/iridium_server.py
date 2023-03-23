eea8bcaf31eb68db82ae25113864a33fe13c8ddc
Ableytner <ableit190030@sr.htlweiz.at>
2023-03-23 15:43:17 +0100

    delete method

--- src/core/iridium_server.py
+++ src/core/iridium_server.py
@@ -90,28 +90,6 @@ class IridiumServer():
         }
         return status
 
-    async def run_coro_threadsafe(self, coro, other_loop, our_loop = None):
-        """Schedules coro in other_loop, awaits until coro has run and returns
-        its result.
-        """
-        loop = our_loop or asyncio.get_event_loop()
-
-        # schedule coro safely in other_loop, get a concurrent.future back
-        # NOTE run_coroutine_threadsafe requires Python 3.5.1
-        fut = asyncio.run_coroutine_threadsafe(coro, other_loop)
-
-        # set up a threading.Event that fires when the future is finished
-        finished = Event()
-        def fut_finished_cb(_):
-            finished.set()
-        fut.add_done_callback(fut_finished_cb)
-
-        # wait on that event in an executor, yielding control to our_loop
-        await loop.run_in_executor(None, finished.wait)
-
-        # coro's result is now available in the future object
-        return fut.result()
-
     async def _handle_client(self, client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):
         logging.debug("Client connected!")
 
